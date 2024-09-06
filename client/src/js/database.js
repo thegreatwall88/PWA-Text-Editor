fd3b1c4b60cc63f28689e7e08758a17175e609b3
Khoi Mai <thegreatwall88@gmail.com>
2024-09-05 20:40:47 -0500

    everything is working, missing readme

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,20 +17,21 @@ export const putDb = async (content) => {
   console.log('Adding content to the database');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('jate', 1);
+  const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('jate', 'readwrite');
+  const tx = jateDb.transaction('jate', 'readwrite');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
 
   // Use the .put() method on the store and pass in the content.
-  const request = store.put({ content });
+  const request = store.put({content});
 
   // Get confirmation of the request.
   const result = await request;
   console.log('Content added to the database', result);
+  return result ? content : null;
 }
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -38,21 +39,20 @@ export const getDb = async () => {
   console.log('GET from the database');
 
   // Create a connection to the database database and version we want to use.
-  const contactDb = await openDB('jate', 1);
+  const jateDb = await openDB('jate', 1);
 
   // Create a new transaction and specify the database and data privileges.
-  const tx = contactDb.transaction('jate', 'readonly');
+  const tx = jateDb.transaction('jate', 'readonly');
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
 
-  // Use the .getAll() method to get all data in the database.
   const request = store.getAll();
 
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
+  return result.length > 0 ? result[result.length - 1].content : null;
 }
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = () => {
       // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Contact Cards'
+        title: 'Just Another Text Editor'
       }),
      
       // Injects our custom service worker
@@ -31,9 +31,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'Contact Cards',
-        short_name: 'Contact',
-        description: 'Never forget your contacts!',
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: 'Take Note With Javacript Syntax Hightlighting',
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: './',
